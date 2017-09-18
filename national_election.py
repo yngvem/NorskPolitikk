4e7e7d8ca856447d6ba8770625566b5bb84819d5
Brage Sekse Aarset <brage.aarset@gmail.com>
2017-09-18 18:13:30 +0200

    begun pseudocode for running a national election

---
+++ national_election.py
@@ -0,0 +1,27 @@
+"""
+TODO: WRITE DOCSTRING
+TODO: WRITE PSEUDOCODE INTO CODE
+"""
+
+__author__ = ['Yngve Mardal Moe', 'Brage Sekse Aarset']
+__email__ = ['yngve.m.moe@gmail.com', 'brage.aarset@gmail.com']
+
+import numpy as np
+import pandas as pd
+
+"""
+PSEUDOCODE:
+def national_election(data, chosen_election_system, election_districts):
+
+    ## election_districts should be a dataframe/series naming X amount of election
+    ## districts and listing the municipalities within them all
+    ## the default one is obv. the 19 counties
+
+    ## election_system should contain information about which election method, tiebreakers, etc.
+
+    if election_system is not the default one:
+        for every district defined in election_districts:
+            sum the parties' votes municipalities, put these sums in a new dataframe/series
+
+
+"""
